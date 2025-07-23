7bb721928370a0a15a64cc0d7c95097beb3271a2
valenjarapatino <143661904+valenjarapatino@users.noreply.github.com>
2025-07-23 15:06:41 -0500

    Add files via upload

---
+++ mmm.js
@@ -0,0 +1,10 @@
+//alert("hola hola");
+//var
+//let
+//const
+
+
+let numero = Number (prompt("escribe un numero"));
+let numero2 =  Number (prompt(" escribe otro un numero"));
+let resultado = numero + numero2
+alert("el numero que esta oculto es:"+resultado);

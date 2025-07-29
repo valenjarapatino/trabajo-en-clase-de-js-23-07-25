1325a1df09acc697e365638bb3e98ef12cdcedcb
valenjarapatino <143661904+valenjarapatino@users.noreply.github.com>
2025-07-29 15:06:34 -0500

    Add files via upload

--- mmm.js
+++ mmm.js
@@ -4,7 +4,37 @@
 //const
 
 
-let numero = Number (prompt("escribe un numero"));
-let numero2 =  Number (prompt(" escribe otro un numero"));
-let resultado = numero + numero2
-alert("el numero que esta oculto es:"+resultado);
+//let numero = Number (prompt("escribe un numero"));
+//let numero2 =  Number (prompt(" escribe otro un numero"));
+//let resultado = numero + numero2
+//alert("el numero que esta oculto es:"+resultado);
+//console.log("el resultado es:"+resultado)
+//document.write("el resultado es:"+resultado)
+
+
+
+
+//trabajo clase
+//let salario = Number(prompt("escribe "))
+//let salud, pension,salarioNeto;
+
+//salud = salario * 4/100
+//pensionNeto = salario - (salud + pension);
+
+//alert (
+    //"el salario del empleado es:" +salario+""
+//)
+
+//.log(
+
+//)
+
+let trainguloBase = Number (prompt("coloca el numero de la base del triangulo"));
+
+let trainguloAltura = Number (prompt("coloca el numero de la altura del triangulo"));
+
+let resultado = trainguloBase*trainguloAltura/2;
+
+alert(
+    "El area del triamgulo es: "+resultado+"\n"
+);

8fe040b00b1423c02fc61cce2340bd37a635f768
valenjarapatino <143661904+valenjarapatino@users.noreply.github.com>
2025-07-30 14:55:22 -0500

    Add files via upload

--- mmm.js
+++ mmm.js
@@ -29,12 +29,79 @@
 
 //)
 
-let trainguloBase = Number (prompt("coloca el numero de la base del triangulo"));
+//ejercicio 1
 
-let trainguloAltura = Number (prompt("coloca el numero de la altura del triangulo"));
+//let trainguloBase = Number (prompt("coloca el numero de la base del triangulo"));
 
-let resultado = trainguloBase*trainguloAltura/2;
+//let trainguloAltura = Number (prompt("coloca el numero de la altura del triangulo"));
+
+//let resultado = trainguloBase*trainguloAltura/2;
+
+//alert(
+    //"El area del triangulo es: "+resultado+"\n"
+//);
+
+
+//ejercicio2
+
+
+//let numero1 = Number (prompt("ingresa un numero"))
+
+//let numero2 = Number (prompt("ingresa otro numero "))
+
+//let sumaResultado = numero1+numero2;
+
+//let restaResultado = numero1-numero2;
+
+//let divisionResultado = numero1/numero2;
+
+//let multiplicacionResultado = numero1*numero2;
+
+
+//alert(
+    //"la suma de los dijitos es: "+sumaResultado+"\n"+
+
+     //"la resta de los dijitos es: "+restaResultado+"\n"+
+
+     // "la division de los dijitos es: "+divisionResultado+"\n"+
+
+      // "la multiplicacion de los dijitos es: "+multiplicacionResultado+"\n"
+    
+//)
+
+
+//ejercicio 3
+
+//let horasTrabajadas = Number (prompt("ingresa las horas trabajadas en un mes")); 20
+
+//let horasExtras = Number (prompt("ingresa las horas extras trabajadas en un mes")); 5
+
+//let valordehornormal = 10000;
+
+//let valordehorextra = 15000;
+
+//let totalhorasNormal = horasTrabajadas * valordehornormal;
+
+//let totalhorasExtra = horasExtras * valordehorextra;
+
+//let sumaTotalhoras = totalhorasNormal + totalhorasExtra;
+
+//alert(
+    //"la hora normal es: "+totalhorasNormal+"\n"+
+    //"la hora extra es: "+totalhorasExtra+"\n"+
+    //"la suma de las horas es: "+sumaTotalhoras
+//);
+
+
+// ejercicio 4
+
+let producto = Number (prompt("ingresa el valor del producto"));
+let SumaIVA = (producto / 19) *100;
+
+let resultadoIVA = SumaIVA;
 
 alert(
-    "El area del triamgulo es: "+resultado+"\n"
+    "el valor del producto es: "+producto+"\n"+
+    "la suma de IVA es: "+SumaIVA+"\n"+
+    "el resultado es: "+resultadoIVA
 );
